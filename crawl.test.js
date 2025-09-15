86d873d20b603bccf5c4b9fa77431b36900487a3
Raihan <raihu2000@gmail.com>
2025-09-15 10:34:10 +0530

    Added getURLFromHTML function and its test cases

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeUrl} = require('./crawl.js');
+const {normalizeUrl, getURLsFromHTML} = require('./crawl.js');
 const {test, expect} = require('@jest/globals');
 
 test('normalizeUrl strip protocol', () => {
@@ -26,5 +26,72 @@ test('normalizeUrl strip http', () => {
     const input = 'http://google.com/path/'
     const actual = normalizeUrl(input);
     const expected = 'google.com/path';
+    expect(actual).toEqual(expected);
+});
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+        <a href = "https://google.com/"> 
+        Welcome to Google
+        </a>
+    </body>
+    </html>`;
+
+    const inputBaseURL = 'https://google.com';
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ['https://google.com/'];
+    expect(actual).toEqual(expected);
+});
+
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+        <a href = "/path/"> 
+        Welcome to Google
+        </a>
+    </body>
+    </html>`;
+
+    const inputBaseURL = 'https://google.com';
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ['https://google.com/path/'];
+    expect(actual).toEqual(expected);
+});
+
+test('getURLsFromHTML both', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+        <a href = "https://google.com/path1/"> 
+        Welcome to Google, path 1
+        </a>
+        <a href = "/path2/"> 
+        Welcome to Google, path 2
+        </a>
+    </body>
+    </html>`;
+
+    const inputBaseURL = 'https://google.com';
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ['https://google.com/path1/', 'https://google.com/path2/'];
+    expect(actual).toEqual(expected);
+});
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+        <a href = "invalid"> 
+        Invalid URL
+        </a>
+    </body>
+    </html>`;
+
+    const inputBaseURL = 'https://google.com';
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [];
     expect(actual).toEqual(expected);
 });
